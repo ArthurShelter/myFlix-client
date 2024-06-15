12b360ed5ff5591a3724753a39b2cbf03a9df731
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-06-14 17:05:09 -0700

    3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,35 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { MovieCard } from "../movie-card/movie-card";
 
 import { MovieView } from "../movie-view/movie-view";
 
+// {
+//   id: 1,
+//   title: "Adaptation",
+//   image:
+//     "https://minimovieposters.com/cdn/shop/products/MPW-101495-a_1024x1024.jpg?v=1571439883",
+//   director: "Spike Jonze",
+// },
+// {
+//   id: 2,
+//   title: "I Heart Huckabees",
+//   image:
+//     "https://imgs.search.brave.com/6hBIz1P3eKQq9Rao9fdLTezMUaAbr8ObXf5YlVCdGNc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cG9zdGVyaXRhdGku/Y29tL3Bvc3RlcnMv/MDAwLzAwMC8wMzIv/MTU4L2ktaGVhcnQt/aHVja2FiZWVzLW1k/LXdlYi5qcGc",
+//   director: "David O. Russell",
+// },
+// {
+//   id: 3,
+//   title: "REDLINE",
+//   image:
+//     "https://m.media-amazon.com/images/I/61lT0bkuHkL._AC_.jpg",
+//   director: "Takeshi Koike",
+// },
+
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Adaptation",
-      image:
-        "https://minimovieposters.com/cdn/shop/products/MPW-101495-a_1024x1024.jpg?v=1571439883",
-      director: "Spike Jonze",
-    },
-    {
-      id: 2,
-      title: "I Heart Huckabees",
-      image:
-        "https://imgs.search.brave.com/6hBIz1P3eKQq9Rao9fdLTezMUaAbr8ObXf5YlVCdGNc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cG9zdGVyaXRhdGku/Y29tL3Bvc3RlcnMv/MDAwLzAwMC8wMzIv/MTU4L2ktaGVhcnQt/aHVja2FiZWVzLW1k/LXdlYi5qcGc",
-      director: "David O. Russell",
-    },
-    {
-      id: 3,
-      title: "REDLINE",
-      image:
-        "https://m.media-amazon.com/images/I/61lT0bkuHkL._AC_.jpg",
-      director: "Takeshi Koike",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  
+  useEffect(() => {
+    fetch("https://movie-db-fullstack-2-27a48700ab77.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            director: doc.director
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
