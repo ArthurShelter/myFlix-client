9355ee6a84fbfe46cec51102cc403a679d5fd492
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-08-29 16:39:28 -0700

    scss for profile-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -6,6 +6,7 @@ import { Row, Col, Form, Button, Card, Container } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
 import { MovieCard } from "../movie-card/movie-card";
+import "./profile-view.scss";
 
 export const ProfileView = ({ user, token, onLoggedOut, movies }) => {
   const [userInfo, setUserInfo] = useState({});
@@ -212,7 +213,6 @@ export const ProfileView = ({ user, token, onLoggedOut, movies }) => {
                   >
                     <MovieCard
                       movie={movie}
-                    // updateAction={() => updateFavoriteMovies(movie.id)}
                     />
                   </Col>
                 ))
